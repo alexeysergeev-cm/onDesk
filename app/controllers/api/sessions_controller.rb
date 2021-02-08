e2f9e882d81f6efc581d4e9cf9133fa2731b1818
AlexeyS <xengawe@gmail.com>
2021-02-08 15:42:01 -0800

    add show view

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,19 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials( params[:user][:useranme],
+                                      params[:user][:password]
+                                    )
+    if @user 
+      render '/api/users/show'
+    else
+      render json: ['Invalid username or password, please try again'], 
+        status: 404
+    end
+  end
+
+  def destroy
+    logout!
+    render json: { message: 'Logout successful'}
+  end
 end
