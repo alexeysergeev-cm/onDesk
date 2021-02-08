88a529662bff983dd9de270e4eaf1c33562aadee
AlexeyS <xengawe@gmail.com>
2021-02-08 15:22:07 -0800

    add session and users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_user, :logged_in?
+  #CRLLL
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+    return nil unless session[:session_token]
+  end
+
+  def require_logged_in!
+    redirect_to api_session_url unless logged_in?
+  end
+
+  def login!(user)
+    @current_user = user 
+    session[:session_token] = user.reset_sesssion_token!
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout!
+    current_user.reset_sesssion_token!
+    session[:session_token] = nil 
+    @current_user = nil 
+  end
+
 end
