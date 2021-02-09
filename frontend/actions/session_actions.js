ab376655e7ddbefe46a164db2dd945a57cedd396
AlexeyS <xengawe@gmail.com>
2021-02-08 21:04:13 -0800

    complete store.js

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 
 const receiveCurrentUser = (currentUser) => ({
@@ -15,7 +15,7 @@ const logoutCurrentUser = ()  => ({
 
 })
 const receiveErrors = (errors) => ({ //array
-  type: RECEIVE_ERRORS,
+  type: RECEIVE_SESSION_ERRORS,
   errors
 })
 
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,7 +1,21 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from "../actions/session_actions";
 
+const _errors ={
+  errors: []
+}
 
-const sessionErrorsReducer = (state = {}, action) => {
-  Object.freeze(state)
+const sessionErrorsReducer = (state = _errors, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS:  
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return _errors;
+    default:
+      return state; 
+  }
 }
 
 export default sessionErrorsReducer;
