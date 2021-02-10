c3d94f5573769b057e8a4adbda1b4d7abbfa2240
AlexeyS <xengawe@gmail.com>
2021-02-09 21:47:47 -0800

    add some CSS to nav_bar!

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,13 +4,12 @@ import { Route } from 'react-router-dom';
 import LoginFormContainer from '../components/sessions/login_form_container';
 import SignupFormContainer from '../components/sessions/signup_form_container';
 import {AuthRoute} from '../util/route_util'
+import NavBarContainer from '../components/nav_bar/nav_bar_container'
 
 const App = () => (
   <div>
-    <header>
-      <h1>onDesk</h1>
-    </header>
-    <Route exact path="/" component={GreetingContainer} />
+    <Route path="/" component={NavBarContainer} />
+    {/* <Route exact path="/" component={GreetingContainer} /> */}
     <AuthRoute path="/login" component={LoginFormContainer} />
     <AuthRoute path="/signup" component={SignupFormContainer} />
   </div>
---
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar  extends  React.Component{
+  render() {
+    const { currentUser, logout } = this.props
+    const display = (
+      <div>
+        {/* <p>Hello, {currentUser.username}</p>
+        <button onClick={logout}>Log Out</button> */}
+        <Link className="btn" to="/signup">Sign Up</Link>
+        <Link className="btn" to="/login">Log In</Link>
+      </div>
+    );
+  
+    return (
+      <header className="nav-bar">
+        <h1 className="logo">onDesk</h1>
+        <div>
+          {display}
+        </div>
+      </header>
+    );
+  }
+};
+
+export default NavBar
