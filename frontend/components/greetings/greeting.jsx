23c7dfa287f3980e722ead3f34572062e21aa184
AlexeyS <xengawe@gmail.com>
2021-02-12 12:51:58 -0800

    add dropdown, work on CurretnUSerEmail

--- frontend/components/greetings/greeting.jsx
+++ frontend/components/greetings/greeting.jsx
@@ -3,19 +3,39 @@ import {Link} from 'react-router-dom'
 
 class Greeting extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.clickDropDown = this.clickDropDown.bind(this)
+  }
 
+  clickDropDown(){
+
+  }
   render(){
-    const { logout, currentUser} = this.props;
+    const { logout, currentUser, email} = this.props;
     let welcome;
     let name;
 
     if (currentUser){
+      name = currentUser.username[0].toUpperCase() + currentUser.username.slice(1)
       welcome = (
       <div>
-          <button className="btn-bg-home loggedIn" onClick={() => logout()}></button>
+          <button className="btn-logout" onClick={this.clickDropDown}>
+
+            <ul className='home-dropdown' >
+              <li>
+                <li>name</li> 
+                <li>{currentUser.email}</li>
+              <hr className="Solid"/>
+              </li>
+              <li>Papers</li>
+              <li>Settings</li>
+              <hr className="Solid"/>
+              <li onClick={() => logout()}>Log out</li>
+            </ul>
+          </button>
       </div>
       )
-      name = currentUser.username[0].toUpperCase() + currentUser.username.slice(1)
     }
 
     return(
