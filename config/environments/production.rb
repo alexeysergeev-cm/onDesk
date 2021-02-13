2815e85f425f5bff4d178cb32ffd87e0f261ef89
AlexeyS <xengawe@gmail.com>
2021-02-13 13:16:04 -0800

    add config.assets.compile = true for logo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
+
+  # add this to show my logo on home page!
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
