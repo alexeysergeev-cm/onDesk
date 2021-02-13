c4aa1518f9ee554d8f49f0a23c8122b304ace00b
AlexeyS <xengawe@gmail.com>
2021-02-12 17:43:33 -0800

    add some styling to a front page

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -19,6 +19,15 @@ class NavBar  extends  React.Component{
             {display}
           </div>
         </nav>
+        <div className='intro-container'>
+          <div className='intro-text'>
+            <h1 className='intro-text-big'> 
+              onDesk helps teams work more collaboratively and get more done.
+            </h1>
+            <p className='lead'>onDesk’s desks, lists, and papers enable teams to organize and prioritize projects in a fun, flexible, and rewarding way.</p> 
+          </div>
+        <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/308998dcb3ed5ab3d01217a4d24ffa03/hero-a.svg" />
+        </div>
       </header>
     );
   }
