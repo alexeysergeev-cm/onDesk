23aeea8e090ff8f25fef652603a47980ad8c6640
AlexeyS <xengawe@gmail.com>
2021-02-13 17:49:04 -0800

    complete desk_form & modal setup

--- frontend/components/desk_form/desk_form.jsx
+++ frontend/components/desk_form/desk_form.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 
-class DeskForm extends React.Component{
+import { withRouter } from 'react-router-dom'
 
+class DeskForm extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      title: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.createDesk(this.state)
+      .then(this.props.closeModal)
+  }
+
+  update(field){
+    e => this.setState({[field]: e.currentTarget.value})
+  }
 
   render(){
     return(
-      <div>
-        
+      <div className='desk-form-container'>
+        <form onSubmit={this.handleSubmit} className='desk-form-box'>
+          <div className='desk-form'>
+              <input type="text"
+                value={this.state.value}
+                onChange={this.forceUpdate('title')}
+                className='desk-input'
+              />
+          </div>
+
+          <input type="submit" className='desk-submit' value={this.props.formType}/>
+        </form>
       </div>
     )
   }
 }
 
-export default DeskForm;
+export default withRouter(DeskForm);
--- frontend/components/desk_form/desk_form_container.js
+++ frontend/components/desk_form/desk_form_container.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { createDesk } from '../../actions/desk_actions'
 import DeskForm from './desk_form'
+import { openModal, closeModal} from '../../actions/modal_actions';
 
 const mSTP = state => ({
-
+  formType: 'Create Desk'
 })
 
 const mDTP = dispatch => ({
-  createDesk: (desk) => dispatch(createDesk(desk))
+  createDesk: (desk) => dispatch(createDesk(desk)),
+  closeModal: () => dispatch(closeModal())
 })
 
-export default connect(null, mDTP)(DeskForm)
+export default connect(mSTP, mDTP)(DeskForm)
