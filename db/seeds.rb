c757a3ee806f11af44160a335dd6d9506b52ea80
AlexeyS <xengawe@gmail.com>
2021-03-27 14:55:05 -0700

    create deskmemeberships seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,27 +10,27 @@ User.destroy_all
 users =[
     { 
         email: "tori@io.com",
-        username: "tori",
+        username: "Tori Helbert",
         password: "123456",
     },
     { 
         email: "mickey@io.com",
-        username: "mickey",
+        username: "Mickey Stone",
         password: "123456",
     },
     { 
         email: "lulu@io.com",
-        username: "lulu",
+        username: "Lulu Thompson",
         password: "123456",
     },
     { 
         email: "charlie@io.com",
-        username: "charlie",
+        username: "Charlie Simpson",
         password: "123456",
     },
     { 
         email: "robert@io.com",
-        username: "robert",
+        username: "Roberto Rodrigez",
         password: "123456",
     },
 ]
@@ -40,20 +40,45 @@ User.create(users)
 Desk.destroy_all
 desks = [
     {
-        title: 'Ideas',
+        title: 'Website Ideas',
         author_id: users[0].id,
+        background_picture: "https://ondesk-dev.s3-us-west-1.amazonaws.com/space.jpg",
     },
     {
-        title: 'Fly to Mars',
-        author_id: users[0].id,
+        title: 'Mission: Mars',
+        author_id: users[1].id,
+        background_picture: "https://ondesk-dev.s3-us-west-1.amazonaws.com/skyscraper.jpeg",
     },
     {
-        title: 'Plan family trip to hawaii',
-        author_id: users[0].id,
+        title: 'Family Trip to Hawaii',
+        author_id: users[2].id,
+        background_picture: "https://ondesk-dev.s3-us-west-1.amazonaws.com/shark.jpeg", 
     },
     {
-        title: 'Hire engineers',
-        author_id: users[0].id,
+        title: 'Wedding',
+        author_id: users[3].id,
+        background_picture: "https://ondesk-dev.s3-us-west-1.amazonaws.com/ocean.jpeg",
     }
 ]
 Desk.create(desks)
+
+DeskMembership.destroy_all
+desk_memberships = [
+    {
+        user_id: users[4].id,
+        desk_id: desk[0].id,
+    },
+    {
+        user_id: users[0].id,
+        desk_id: desk[1].id,
+    },
+    {
+        user_id: users[0].id,
+        desk_id: desk[2].id,,
+    },
+    {
+        user_id: users[0].id,
+        desk_id: desk[3].id,,
+    },
+]
+DeskMembership.create(desk_memberships)
