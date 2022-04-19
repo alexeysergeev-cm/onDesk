c326f49d8eed79c3122c58b3260a18fb524b110e
Alexey Sergeev <xengawe@gmail.com>
2022-04-18 21:06:15 -0700

    add icons and classnames and bulma

--- frontend/components/desk_show/membersList.js
+++ frontend/components/desk_show/membersList.js
@@ -3,7 +3,10 @@ import { useDispatch } from "react-redux";
 import { searchItems } from "../../actions/search_actions";
 import { Link } from "react-router-dom";
 import { useEventListener } from "../../hooks/useEventListener";
-import './memberList.scss'
+import "./memberList.scss";
+import classnames from "classnames"
+
+const COLORS = ["#6157bf", "#cabd30", "#d92828", "#77b974"];
 
 function MembersList({ data }) {
   const [members, setMembers] = useState([]);
@@ -16,9 +19,23 @@ function MembersList({ data }) {
         return (
           <button
             key={member.id}
-            className="button is-info is-light member-icon"
+            className={classnames("button is-info is-light member-icon", {
+              "with-image": !!member.photoUrl
+            })}
+            style={{
+              backgroundColor:
+                COLORS[Math.floor(Math.random() * COLORS.length)],
+              backgroundImage: `url(${member.photoUrl})`,
+            }}
           >
-            {member.username}
+            {!member.photoUrl ? (
+              <>
+                {member.username.split(" ")[0][0] +
+                  member.username.split(" ")[1][0]}
+              </>
+            ) : (
+              null
+            )}
           </button>
         );
       })}
