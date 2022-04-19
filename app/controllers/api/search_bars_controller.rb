e87aad3cf17eb297c27ebf41fbbba9a1fea9a034
Alexey Sergeev <xengawe@gmail.com>
2022-04-19 13:22:02 -0700

    search only your desks

--- app/controllers/api/search_bars_controller.rb
+++ app/controllers/api/search_bars_controller.rb
@@ -1,20 +1,28 @@
 class Api::SearchBarsController < ApplicationController
+  before_action :get_user_related_data, only: %i[search]
 
   def index
-
   end
   
   def search
-    if params[:q] === '' 
-      @desks = [];
-      @lists = [];
-      @papers = [];
-      render :index
+    if params[:q] == '' || params[:q].nil?
+      head :ok
     else
-      @desks = Desk.where('lower(title) LIKE ?', '%' + params[:q].downcase + '%');
-      @lists = List.where('lower(title) LIKE ?', '%' + params[:q].downcase + '%');
-      @papers = Paper.where('lower(title) LIKE ?', '%' + params[:q].downcase + '%');
+      query = params[:q].downcase
+      
+      @desks = @d.where('lower(title) LIKE ?', '%' + query + '%')
+      @lists =  @l.where('lower(lists.title) LIKE ?', '%' + query + '%')
+      @papers = @p.where('lower(papers.title) LIKE ?', '%' + query + '%')
+
       render :index
     end
+  end
+
+  private
+
+  def get_user_related_data
+    @d = Desk.joins(:desk_memberships).where("desk_memberships.user_id = ?", current_user.id)
+    @l = List.joins(:desk).where("desks.id IN (?)", @d.ids)
+    @p = Paper.joins(:list).where("lists.id IN (?)", @l.ids)
   end
 end
--- app/views/api/search_bars/index.json.jbuilder
+++ app/views/api/search_bars/index.json.jbuilder
@@ -1,40 +1,21 @@
 
-# if !@desks.nil? 
-#   @desks.each do |desk|
-#     debugger
-#     json.extract! desk, :id, :title, :author_id, :background_picture, :list_order
-#   end
-# end
-
-if !@desks.nil?
-  json.desks do
-    json.array!(@desks) do |desk|
-      json.id desk.id
-      json.title desk.title
-    end
+json.desks do
+  json.array!(@desks) do |desk|
+    json.id desk.id
+    json.title desk.title
   end
 end
 
-if !@lists.nil?
-  json.lists do
-    json.array!(@lists) do |list|
-      json.id list.desk_id
-      json.title list.title
-    end
+json.lists do
+  json.array!(@lists) do |list|
+    json.id list.desk_id
+    json.title list.title
   end
 end
 
-if !@papers.nil?
-  json.papers do
-    json.array!(@papers) do |paper|
-      #need to get desk id!
-      List.all.each do |list|
-        if list.id == paper.list_id
-          json.id list.desk_id
-        end
-      end
-      
-      json.title paper.title
-    end
+json.papers do
+  json.array!(@papers) do |paper|
+    json.id List.find(paper.list_id).desk_id
+    json.title paper.title
   end
 end
