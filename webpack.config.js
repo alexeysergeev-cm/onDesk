759e92c3f0261305b4cb757da94c4bd2d61812f0
Alexey Sergeev <xengawe@gmail.com>
2022-05-03 11:03:55 -0700

    css landing page

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   devtool: "source-map",
