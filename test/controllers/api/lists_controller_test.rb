7a501add739ca2fd146b3f7f39245659831284fb
Alexey Sergeev <xengawe@gmail.com>
2022-05-10 15:02:59 -0700

    add gem webdrivers, remove tests

--- test/controllers/api/lists_controller_test.rb
+++ test/controllers/api/lists_controller_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
 
 class Api::ListsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get api_lists_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get api_lists_create_url
+  #   assert_response :success
+  # end
 
-  test "should get update" do
-    get api_lists_update_url
-    assert_response :success
-  end
+  # test "should get update" do
+  #   get api_lists_update_url
+  #   assert_response :success
+  # end
 
-  test "should get destroy" do
-    get api_lists_destroy_url
-    assert_response :success
-  end
+  # test "should get destroy" do
+  #   get api_lists_destroy_url
+  #   assert_response :success
+  # end
 
-  test "should get index" do
-    get api_lists_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get api_lists_index_url
+  #   assert_response :success
+  # end
 
 end
