c61e35b2d89ef9f7cfa6164c06b6c66890e9f3a4
Alexey Sergeev <xengawe@gmail.com>
2022-05-13 13:51:22 -0700

    ty test

---
+++ test/channels/desk_channel_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class DeskChannelTest < ApplicationCable::Channel
+  test "subscribes and stream for desk" do
+    # Simulate a subscription creation by calling `subscribe`
+    subscribe desk: "1"
+
+    # You can access the Channel object via `subscription` in tests
+    assert subscription.confirmed?
+    assert_has_stream "desk_1"
+  end
+end
