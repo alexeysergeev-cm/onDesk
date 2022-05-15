c7dfe695cb1fc4e1ba8c9c47eabe59a21325bc2e
Alexey Sergeev <xengawe@gmail.com>
2022-05-14 21:06:33 -0700

    use let

--- spec/models/desk_spec.rb
+++ spec/models/desk_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Desk, type: :model do
-  subject(:user) { User.create(username: "Bob Fisher", password: "some_password", email: "fisher@io.com") }
-  subject(:desk) { Desk.new(title: "title", author_id: user.id, background_picture: "") }
+  let(:author) { User.create(username: "Bob Fisher", password: "some_password", email: "fisher@io.com") }
+  subject(:desk) { Desk.new(title: "title", author_id: author.id, background_picture: "") }
   
   describe "Desk Creation / Validation" do 
     it { should validate_uniqueness_of(:title) }
